ee25ee52c257a4fd6fa2fb9d1e8e9029909b13ed
Nastya Sivko <grintsevichnastya@gmail.com>
2020-03-30 16:26:13 +0300

    test for method getSalaryWithoutTax

--- service/src/test/java/TestGetSalary.java
+++ service/src/test/java/TestGetSalary.java
@@ -1,3 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import static by.itacademy.utils.CalculatorSalary.getSalaryWithoutTax;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertFalse;
+
 public class TestGetSalary {
 
+    @Test
+    public void getSalaryTest() {
+        int salary = 500;
+        int tax = 10;
+
+        int result = getSalaryWithoutTax(salary, tax);
+
+        assertEquals(result, 375);
+    }
+
+    @Test
+    public void getSalaryTest2() {
+        int salary = 1000;
+        int tax = 0;
+
+        int result = getSalaryWithoutTax(salary, tax);
+
+        assertEquals(result, 425);
+    }
 }
