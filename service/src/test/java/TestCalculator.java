d4a3ec6e75e0579393c31cb853dbfab971a3792c
Nastya Sivko <grintsevichnastya@gmail.com>
2020-03-30 16:12:50 +0300

    test for method calculatorPercent

---
+++ service/src/test/java/TestCalculator.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static by.itacademy.utils.CalculatorSalary.calculatorPercent;
+import static junit.framework.Assert.assertEquals;
+
+public class TestCalculator {
+
+    @Test
+    public void Calculator(){
+        calculatorPercent(520, 10);
+    }
+
+    @Test
+    public void CalculatorTest(){
+        int salary = 240;
+        int tax = 15;
+
+        int result = calculatorPercent(salary, tax);
+
+        assertEquals(result, 36);
+    }
+}
